3232a3588cf60e8f59a0928c74f739a2d5c06fec
Sarpotter326 <sarah.e.potter326@gmail.com>
2025-10-01 15:58:06 -0700

    Fix import quotes for product catalog
    Correct mismatched quote in import statement to ensure proper syntax and successful build.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import Image from 'next/image';
-import products from '../data/catalog.js";
+import products from '../data/catal'g.js";
 
 
 function SidebarNav({ open, onClose }) {
