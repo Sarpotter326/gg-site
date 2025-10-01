7ab815eb4a4996d525f153549fb1cfe549dc4761
Sarpotter326 <sarah.e.potter326@gmail.com>
2025-10-01 15:57:02 -0700

    Use JS module for product catalog import
    Update index.js to import products from '../data/catalog.js' instead of JSON, ensuring Next.js builds successfully.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import Image from 'next/image';
-import products from '../data/catalog.json';
+import products from '../data/catalog.js";
 
 
 function SidebarNav({ open, onClose }) {
